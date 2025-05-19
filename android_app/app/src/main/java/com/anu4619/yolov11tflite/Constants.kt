8ab876ac042484649214650eff5ad505be0265ac
Anushka Nagdeote <anushka4619@gmail.com>
2025-05-19 20:27:53 +0530

    experimenting with yolo11n_float32 model

--- android_app/app/src/main/java/com/anu4619/yolov11tflite/Constants.kt
+++ android_app/app/src/main/java/com/anu4619/yolov11tflite/Constants.kt
@@ -1,6 +1,6 @@
 package com.anu4619.yolov11tflite
 
 object Constants {
-    const val MODEL_PATH = "yolo11n_integer_quant.tflite"
+    const val MODEL_PATH = "yolo11n_float32.tflite"
     const val LABELS_PATH = "labels.txt"
 }
